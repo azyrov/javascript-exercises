d611901b1f36c71d57f69a58f28ccfade7ea96da
azyrov <for28@yahoo.com>
2026-08-25 01:08:24 +0000

    Add code for all calculator solutions except for factorials

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+  return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce(((sum, current) => sum + current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(((sum, num) => sum * num), 1)
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+  return numOne ** numTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  
+  let sum = 0;
+
+  for (let i = num; i > 2; --i) {
+
+    let reduced = (i - 1);
+
+    if (num === i) {
+      sum += num * reduced;
+    } else if (num > i) {
+      sum += sum * (reduced - 1);
+    } 
+  }
+
+  return sum
 };
 
 // Do not edit below this line
